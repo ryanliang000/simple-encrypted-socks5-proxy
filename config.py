80167b639c0a9bbd58d436a14c3b406d7d3c41e3
ryanliang000 <mail>
2019-07-04 21:30:42 +0800

    change server port and encode char

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-server_bind_addr = ("0.0.0.0", 3030)
+server_bind_addr = ("0.0.0.0", 28101)
 local_bind_addr = ("127.0.0.1", 7070)
 
 server_proxy_addr = ("your_remote_proxy_ip", server_bind_addr[1])
 
 def encode(c):
-  return chr(c^200)
+  return chr(c^231)
